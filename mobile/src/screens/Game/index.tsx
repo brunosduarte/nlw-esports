9ec16df51a738988a57e8b0d50723679d6468b00
Bruno Duarte <brunosduarte@hotmail.com>
2022-11-15 05:03:11 -0300

    fifthclass - finished mobile

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -13,22 +13,27 @@ import { GameParams } from '../../@types/navigation';
 
 import { Heading } from '../../components/Heading';
 import { Background } from '../../components/Background';
+import { DuoMatch } from '../../components/DuoMatch';
 import { DuoCard, DuoCardProps } from '../../components/DuoCard';
-
-interface RouteParams {
-  id: string;
-  title: string;
-  bannerUrl: string;
-}
 
 export function Game() {
   const [duos, setDuos] = useState<DuoCardProps[]>([]);
+  const [discordDuoSelected, setDiscordDuoSelected] = useState('');
+
   const navigation = useNavigation();
   const route = useRoute();
   const game = route.params as GameParams;
   
   function handleGoBack() {
     navigation.goBack();
+  }
+
+  async function getDiscordUser(adsId: string) {
+    fetch(`http://192.168.0.9:3333/ads/${adsId}/discord`)
+    .then(response => response.json())
+    .then(data => {
+      setDiscordDuoSelected(data.discord);
+    });
   }
 
   useEffect(() => {
@@ -76,7 +81,7 @@ export function Game() {
           renderItem={({ item }) => (
             <DuoCard 
               data={item}
-              onConnect={() => { }}
+              onConnect={() => getDiscordUser(item.id)}
             />  
           )}
           horizontal
@@ -88,6 +93,12 @@ export function Game() {
               Não há anúncios publicados ainda.
             </Text>  
           )}
+        />
+
+        <DuoMatch
+          visible={discordDuoSelected.length > 0}
+          discord={discordDuoSelected}
+          onClose={() => setDiscordDuoSelected('')}
         />
 
       </SafeAreaView>
